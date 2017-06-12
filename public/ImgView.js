94e7cef4b750d71462e7952f81f9066eb80aaba1
Euan Fullarton <euanfullarton7@gmail.com>
2017-06-12 20:25:38 +0100

    tidied up the imgView functions

--- public/ImgView.js
+++ public/ImgView.js
@@ -4,18 +4,20 @@ var ImgView = function() {
 
 ImgView.prototype = {
 
+  changeImage:function(imgToHide, imgToShow){
+    imgToHide.style.opacity = '0';
+    imgToShow.style.opacity = '100';
+  },
+
   nextImg: function(){
 
     var mainImages = document.getElementsByClassName('main-image');
-
     for (var i=0;i<mainImages.length;i+=1){
       if ((mainImages[i].style.opacity === '100') && (!mainImages[i + 1])){
-        mainImages[i].style.opacity = '0';
-        mainImages[i - 2].style.opacity = '100';
+        ImgView.changeImage(mainImages[i], mainImages[i-2]);
       }
       else if (mainImages[i].style.opacity === '100'){
-        mainImages[i].style.opacity = '0';
-        mainImages[i + 1].style.opacity = '100';
+        ImgView.changeImage(mainImages[i], mainImages[i+1]);
         return;
       }
     }
@@ -27,12 +29,10 @@ ImgView.prototype = {
 
     for (var i=mainImages.length-1;i>=0;i-=1){
       if ((mainImages[i].style.opacity === '100') && (!mainImages[i - 1])){
-        mainImages[i].style.opacity = '0';
-        mainImages[ i + 2 ].style.opacity = '100';
+        ImgView.changeImage(mainImages[i], mainImages[i+2]);
       }
       else if (mainImages[i].style.opacity === '100'){
-        mainImages[i].style.opacity = '0';
-        mainImages[i - 1].style.opacity = '100';
+        ImgView.changeImage(mainImages[i], mainImages[i-1]);
         return;
       }
     }
